5d72cba3d7235abbad607c9bc141d5ffb2974dd2
Evan Pavone <evanrpavone@gmail.com>
2020-08-25 15:50:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,12 +8,10 @@ INSERT INTO books (id, title, year, series_id) VALUES
 (5, "Waves", 2003, 2),
 (6, "Vie Bold", 2005, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
-(1, "Oli", "Sleepwalking", "direwolf", 1, 1),
-(2, "Matt", "I'm a Drummer", "human", 1, 1),
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Oli", "Sleepwalking", "direwolf", 1, 1),(2, "Matt", "I'm a Drummer", "human", 1, 1),
 (3, "Monster", "Blegh", "human", 1, 1),
 (4, "Danny", "This is the new Era", "human", 1, 1),
-(4, "Ben", "Songwriter", "human", 1, 1),
-(4, "Jordan", "I like a little bit of screams", "direwolf", 1, 1),
-(4, "Sean", "I am weird", "human", 1, 1),
-(4, "Tori", "Wife of danny", "human", 1, 1);
+(5, "Ben", "Songwriter", "human", 1, 1),
+(6, "Jordan", "I like a little bit of screams", "direwolf", 1, 1),
+(7, "Sean", "I am weird", "human", 1, 1),
+(8, "Tori", "Wife of danny", "human", 1, 1);
