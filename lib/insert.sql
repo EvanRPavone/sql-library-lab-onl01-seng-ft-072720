e83e8cf70cb8329356da7d9f320a160dec7f70d4
Evan Pavone <evanrpavone@gmail.com>
2020-08-25 15:43:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Martyr", 1, 1), (2, "The Descent", 2, 2);
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Sempriternal", 1996, 1), (2, "The Horizon", 1998, 1), (3, "Currents", 2000, 1), (4, "Polaris", 2002, 2), (5, "Waves", 2003, 2), (6, "Vie Bold", 2005, 2);
